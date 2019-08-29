38b74fef498d512e067eabeffa85dce3c522624f
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-08-29 15:40:12 -0700

    added html and css

---
+++ models/login.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+    const User = sequelize.define("User", {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        fullname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userid: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+}
---
+++ server.js
@@ -0,0 +1,53 @@
+const mysql = require('mysql');
+const express = require('express');
+// const session = require('express-session');
+// const bodyParser = require('body-parser');
+const path = require('path');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+
+const PORT = process.env.PORT || 8080;
+
+passport.use(new Strategy(
+    function(username, password, cb) {
+      db.users.findByUsername(username, function(err, user) {
+        if (err) { return cb(err); }
+        if (!user) { return cb(null, false); }
+        if (user.password != password) { return cb(null, false); }
+        return cb(null, user);
+      });
+    }));
+
+    passport.serializeUser(function(user, cb) {
+        cb(null, user.id);
+      });
+      
+      passport.deserializeUser(function(id, cb) {
+        db.users.findById(id, function (err, user) {
+          if (err) { return cb(err); }
+          cb(null, user);
+        });
+      });
+      
+
+const app = express();
+/*
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));*/
+// The above is the same as the below
+
+app.use(require('morgan')('combined'));
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
