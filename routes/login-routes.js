95a3dc893bee7c6c722327730fd4108edb634485
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-08-29 15:59:20 -0700

    set up login routes and fixed db name

--- routes/login-routes.js
+++ routes/login-routes.js
@@ -1,2 +1,31 @@
 
 const db = require("../models");
+
+
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res){
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+  
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+  
+app.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
