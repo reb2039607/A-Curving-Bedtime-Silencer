018c9c0b106f9a6cc4cf56bbc637cc4eef589b75
Jogeswararao Kolla <jogeskolla@gmail.com>
2019-08-29 20:26:48 -0700

    Update the model names

--- models/login.js
+++ models/login.js
@@ -1,21 +1,21 @@
 module.exports = function(sequelize, DataTypes) {
-    const User = sequelize.define("User", {
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        fullname: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    });
-    return User
-}
+  const User = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    fullname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  return User;
+};
--- models/userTask.js
+++ models/userTask.js
@@ -1,34 +1,34 @@
 module.exports = function(sequelize, DataTypes) {
-    const userTask = sequelize.define("User", {
-        userid: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                len: [1, 11]
-            }
-        },
-        taskid: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                len: [1, 11]
-            }
-        },
-        actualstartDate: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        actualenddate: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        taskstatus: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1, 10]
-            }
-        }
-    });
-    return userTask;
-}
+  const userTask = sequelize.define("userTask", {
+    userid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1, 11]
+      }
+    },
+    taskid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1, 11]
+      }
+    },
+    actualstartDate: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    actualenddate: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    taskstatus: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 10]
+      }
+    }
+  });
+  return userTask;
+};
