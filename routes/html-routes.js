7bdc3a50bb6c252b5771c44e25b284ce18ae9ba2
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-09-05 16:07:58 -0700

    fixed file path

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,20 +3,23 @@ const path = require("path");
 
 module.exports = function(app) {
   app.get("/createTask", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/createTask.html"));
+    res.sendFile(path.join(__dirname, "../createTask.html"));
   });
 
   app.get("/alltasks", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/allTask.html"));
+    res.sendFile(path.join(__dirname, "../allTask.html"));
   });
 
   app.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "../login.html"));
   });
   app.get("/signup", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../signup.html"));
   });
   app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../index.html"));
+  });
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
   });
 };
